71d7519facd917ac0a3ec045769908b06cf8510c
Gareth Fleming <gareth.fleming@instil.co>
2015-04-29 13:37:35 +0100

    check for username already registered. remove user from registered list when they disconnect.

--- index.js
+++ index.js
@@ -32,13 +32,23 @@
 
     // handle websocket connections
     io.on('connection', function(socket) {
+        var user;
         socket.on('register', function(data) {
-            var user = data.username;
-            users[user] = socket;
-            socket.emit('registered', {'username': user, 'currentState': current_state, 'uuid': uuid.v4()});
+            user = data.username;
+            if (get_socket_for_user(user) !== fake_socket) {
+                var reason = '"' + user + '" is already registered';
+                socket.emit('register_fail', {'reason':reason});
+            }
+            else {
+                users[user] = socket;
+                socket.emit('registered', {'username':user, 'currentState':current_state, 'uuid':uuid.v4()});
+            }
         });
         socket.on('guess', function(data) {
             guesses[data.colour] = data.username;
+        });
+        socket.on('disconnect', function() {
+            delete(users[user]);
         });
     });
 
